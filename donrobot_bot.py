c951908291eca3a449a7ac5d991242b4f8490e83
Rubén Orta <xrx0xmx@protonmail.com>
2016-07-20 19:15:20 +0200

    returns vocoder after command

--- donrobot_bot.py
+++ donrobot_bot.py
@@ -1,15 +1,81 @@
 import sys
 import time
-import telepot
 import re
+from pprint import pprint
 
 import glob, os, json
 
 from pydub import AudioSegment
 from pydub.playback import play
+from pydub.effects import invert_phase, compress_dynamic_range
 
-from pprint import pprint
+import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+
+import sys
+from aubio import source, sink, pvoc
+
+def vocoder(file_id):
+	
+	in_file = '/tmp/don_robot/output/' + file_id + ".mp3"
+	out_file = '/tmp/don_robot/output/' + file_id + "-voc.mp3"
+
+	samplerate = 44100
+	f = source(in_file, samplerate, 256)
+	g = sink(out_file, samplerate)
+	total_frames, read = 0, 256
+
+	win_s = 512                          # fft size
+	hop_s = win_s // 2                   # hop size
+	pv = pvoc(win_s, hop_s)              # phase vocoder
+
+	while read:
+		samples, read = f()
+		spectrum = pv(samples)           # compute spectrum
+		spectrum.norm *= .5             # reduce amplitude a bit .8
+		spectrum.phas[:] = 0.            # zero phase
+		new_samples = pv.rdo(spectrum)   # compute modified samples
+		g(new_samples, read)             # write to output
+ 		total_frames += read
+
+	format_str = "read {:d} samples from {:s}, written to {:s}"
+	print(format_str.format(total_frames, f.uri, g.uri))
+	return out_file
+
+def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
+
+	trim_ms = 0
+	while sound[trim_ms:trim_ms+chunk_size].dBFS < silence_threshold:
+		trim_ms += chunk_size
+	return trim_ms
+
+def clean_audio(file_id):
+
+	print "Processing audio"
+	file = origin_path + file_id
+	sound = AudioSegment.from_ogg(file)
+
+	start_trim = detect_leading_silence(sound)
+	end_trim = detect_leading_silence(sound.reverse())
+	duration = len(sound)    
+	sound = sound[start_trim:duration-end_trim]
+	sound = invert_phase(sound)
+
+	output_file = "/tmp/don_robot/output/" + file_id + ".mp3"
+	file_handle = sound.export( output_file, format="mp3")
+	print("Output File....")
+	pprint(file_handle)
+
+def remove_files(file_id):
+
+	file = "/tmp/don_robot/output/" + file_id + ".mp3"
+	os.remove(file);
+	file = "/tmp/don_robot/" + file_id + ".json"
+	os.remove(file);
+	file = "/tmp/don_robot/" + file_id
+	os.remove(file);
+	file = "/tmp/don_robot/output/" + file_id + "-voc.mp3"
+	os.remove(file);
 
 
 def on_voice_msg(msg):
@@ -45,33 +111,27 @@ def on_chat_msg(msg):
 		messages.append(msg)
 
     if content_type == 'text':
+
+    	finder = re.compile(ur'/debug')
+        if (re.search(finder, msg['text'])):
+        	debug = TRUE
+        	for my_msg in messages:
+        		bot.sendMessage(chat_id, 'file_id: ' + my_msg['voice']['file_id'] + " size: " + str(my_msg['voice']['file_size']))
+
     	finder = re.compile(ur'/list')
         if (re.search(finder, msg['text'])):
         	for my_msg in messages:
         		bot.sendMessage(chat_id, 'file_id: ' + my_msg['voice']['file_id'] + " size: " + str(my_msg['voice']['file_size']))
-				#bot.sendMessage(chat_id, 'file_id: ' + my_msg['voice']['file_id'])
-				#print(my_msg['voice']['file_size']);
 
         finder = re.compile(ur'/process')
         if (re.search(finder, msg['text'])):
         	msg_to_process = messages.pop()
-        	file = origin_path + msg_to_process['voice']['file_id']
-        	print("Processing: " + file)
-        	song = AudioSegment.from_ogg(file)
-        	play(song)
-        	bot.sendMessage(chat_id, 'Playing....' + file)
-
-    	finder = re.compile(ur'/nariz')
-        if (re.search(finder, msg['text'])):
-        	bot.sendMessage(chat_id, 'El afiladoooooooooooooooooooooor')
-        	result = bot.sendVoice(chat_id, open('afilador.mp4', 'rb'))
-        	pprint(result)
-
-    	finder = re.compile(ur'llamamos')
-        if (re.search(finder, msg['text'])):
-    		keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Call Afilador', callback_data='press')],
-            ])
+        	clean_audio(msg_to_process['voice']['file_id'])
+        	result_file = vocoder(msg_to_process['voice']['file_id'])
+        	bot.sendMessage(chat_id, 'Don Robot dice....')
+        	print("Sending..." + result_file)
+        	bot.sendVoice(chat_id, open(result_file, 'rb'))
+        	remove_files(msg_to_process['voice']['file_id'])
 
     if content_type == 'new_chat_member':
         bot.sendMessage(chat_id, "Wellcome to this awesome group!")
